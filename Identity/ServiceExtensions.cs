c04a5790a851c8b0ec21a1e2ac3905bf57ad2494
Nicolas Rodriguez <nicolas_rodriguez_94@hotmail.com>
2022-02-21 17:33:49 -0300

    Cambios 21/2

---
+++ Identity/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Identity.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity
+{
+    public static class ServiceExtensions
+    {
+        public static void AddIdentityInfraestruture(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IdentityContext>(options => 
+            options.UseSqlServer(
+                configuration.GetConnectionString("IdentityConnection"),
+                b => b.MigrationsAssembly(typeof(IdentityContext).Assembly.FullName)));
+        }
+    }
+}
